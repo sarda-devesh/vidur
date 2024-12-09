e285fcbd1870921781cf7b7ba284df8bfe55a4d0
poal023 <91771571+poal023@users.noreply.github.com>
2024-12-09 02:31:16 -0600

    run-vidur-jobs: updated graph_cdf

--- run-vidur-jobs.py
+++ run-vidur-jobs.py
@@ -88,8 +88,26 @@ def plot_cdfs(data_frames, metric, labels):
             min_val = df[metric].min()
     for df, label in zip(data_frames, labels):
         #df[metric] = normalize(df[metric], min_val, max_val)
+        print(label)
         smoothed_data = pd.Series(df[metric]).rolling(window=5).mean()
-        plt.plot(smoothed_data, df['cdf'], label=label)
+        if ("llama3" in label):
+            marker = '-'
+        elif ("llama2" in label):
+            marker = '--'
+        if ("rr" in label):
+            color = 'orange'
+        elif ("rand" in label):
+            color = 'red'
+        elif ("lor" in label):
+            color = 'blue'
+        elif ("ib" in label):
+            color = 'green'
+        elif ("ob" in label):
+            color = 'purple'
+        else:
+            color = 'black'
+
+        plt.plot(smoothed_data, df['cdf'], label=label, linestyle=marker, color=color)
     plt.legend(labels)
     plt.xlabel(f"{metric} (s)")
     plt.ylabel('cdf')
@@ -151,8 +169,9 @@ def plot_metrics(config, base_output_dir, schedulers):
             if (n == "avg_mfu"):
                 plot_avg_mfu(llms, labels)
                 continue
-            for llm in llms:
+            for llm, label  in zip(llms, labels):
                 file = f'{llm}/{n}.csv'
+                print(llm, file)
                 curr_df = pd.read_csv(file) 
                 data_frames.append(curr_df)
             if ("time_series" in n):
